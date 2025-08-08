25309ea29171d62b6385279bde98ab9efa2275af
Karthik G Sharma <158193184+Karthikgs3@users.noreply.github.com>
2025-08-08 23:54:24 +0530

    Update drizzle.config.js

--- drizzle.config.js
+++ drizzle.config.js
@@ -5,6 +5,6 @@ export default defineConfig({
   schema: "./utils/schema.js",
   out: "./drizzle",
   dbCredentials: {
-    url:'postgresql://neondb_owner:npg_AMNfb6e4VPUG@ep-purple-cake-aew7zq5r-pooler.c-2.us-east-2.aws.neon.tech/AI-Interview?sslmode=require&channel_binding=require',
+    url: process.env.DB_URL,
   }
 });
